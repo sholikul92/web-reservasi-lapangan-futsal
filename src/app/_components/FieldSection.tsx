807b85089b67a0495394ba0694af4e01a88fcd69
Happy City <happycity805@gmail.com>
2025-06-09 17:25:41 +0700

    add link

--- src/app/_components/FieldSection.tsx
+++ src/app/_components/FieldSection.tsx
@@ -1,41 +1,60 @@
-import Image from "next/image";
-import { Button } from "@heroui/button";
+import Image from 'next/image';
+import { Button } from '@heroui/button';
+import Link from 'next/link';
 
 const fields = [
   {
     id: 1,
-    name: "Lapangan A",
-    price: "150.000",
-    imageUrl: "/lapangan-1.jpg",
+    name: 'Lapangan A',
+    price: '150.000',
+    imageUrl: '/lapangan-1.jpg',
   },
   {
     id: 2,
-    name: "Lapangan B",
-    price: "150.000",
-    imageUrl: "/lapangan-2.jpg",
+    name: 'Lapangan B',
+    price: '150.000',
+    imageUrl: '/lapangan-2.jpg',
   },
   {
     id: 3,
-    name: "Lapangan C",
-    price: "100.000",
-    imageUrl: "/lapangan-3.jpg",
+    name: 'Lapangan C',
+    price: '100.000',
+    imageUrl: '/lapangan-3.jpg',
   },
 ];
 
 export default function FieldSection() {
   return (
     <section id='fields' className='p-4 my-8 scroll-mt-20'>
-      <h1 className='mb-8 text-2xl font-semibold text-center md:text-4xl'>Daftar Lapangan</h1>
+      <h1 className='mb-8 text-2xl font-semibold text-center md:text-4xl'>
+        Daftar Lapangan
+      </h1>
       <div className='grid grid-cols-1 gap-4 md:grid-cols-3 md:mx-60'>
         {fields.map((field) => (
-          <div key={field.id} className='overflow-hidden bg-white shadow-lg rounded-xl'>
-            <Image src={field.imageUrl} alt='lapangan-A' width={200} height={150} className='w-full' priority />
+          <div
+            key={field.id}
+            className='overflow-hidden bg-white shadow-lg rounded-xl'
+          >
+            <Image
+              src={field.imageUrl}
+              alt='lapangan-A'
+              width={200}
+              height={150}
+              className='w-full'
+              priority
+            />
             <div className='flex flex-col gap-2 p-4 md:flex-row md:items-end'>
               <div className='flex-1 text-black/70'>
                 <p className='text-xl font-semibold'>{field.name}</p>
                 <p>Rp. {field.price} / Jam</p>
               </div>
-              <Button color='primary'>Booking Sekarang</Button>
+              <Link
+                href='/dashboard/player/booking'
+                aria-label='link booking'
+                className='w-full border'
+              >
+                <Button color='primary' className="w-full">Booking Sekarang</Button>
+              </Link>
             </div>
           </div>
         ))}
--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,6 +1,7 @@
-"use client";
-import { useState, useEffect } from "react";
-import { HiOutlineMenuAlt1 } from "react-icons/hi";
+'use client';
+import { useState, useEffect } from 'react';
+import { HiOutlineMenuAlt1 } from 'react-icons/hi';
+import Link from 'next/link';
 
 export default function Header() {
   const [scrolled, setScrolled] = useState<boolean>(false);
@@ -12,46 +13,56 @@ export default function Header() {
       setIsMenuOpen(false);
     };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.addEventListener('scroll', handleScroll);
   }, []);
 
   return (
     <header
       id='header'
-      className={`p-4 fixed left-0 right-0 z-50 transition-colors duration-75 ${scrolled ? "bg-white shadow-md" : "bg-transparent"}`}
+      className={`p-4 fixed left-0 right-0 z-50 transition-colors duration-75 ${
+        scrolled ? 'bg-white shadow-md' : 'bg-transparent'
+      }`}
     >
       <nav className='flex flex-col gap-2 text-black md:flex-row md:justify-around'>
         <div className='flex items-center justify-between md:justify-start'>
-          <h1 className={`text-xl font-semibold ${!scrolled && "text-white"}`}>
+          <h1 className={`text-xl font-semibold ${!scrolled && 'text-white'}`}>
             <span className='text-primary'>Unipi</span>Futsal
           </h1>
-          <HiOutlineMenuAlt1 className='text-2xl text-primary md:hidden' onClick={() => setIsMenuOpen(!isMenuOpen)} />
+          <HiOutlineMenuAlt1
+            className='text-2xl text-primary md:hidden'
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          />
         </div>
         <ul
-          className={`${isMenuOpen ? "flex" : "hidden"} ${
-            !scrolled && "text-white"
+          className={`${isMenuOpen ? 'flex' : 'hidden'} ${
+            !scrolled && 'text-white'
           } flex-col md:flex md:flex-row items-start md:justify-between md:w-1/2 gap-4 md:gap-6`}
         >
           <li className='w-full'>
-            <a href='#' className='block'>
+            <Link href='#' className='block' aria-label='link beranda'>
               Beranda
-            </a>
+            </Link>
           </li>
           <li className='w-full'>
-            <a href='#fields' className='block'>
+            <Link href='#fields' className='block' aria-label='link lapangan'>
               Lapangan
-            </a>
+            </Link>
           </li>
           <li className='w-full'>
-            <a href='#form-cek-jadwal' className='block'>
-              Cek Jadwal
-            </a>
-          </li>
-          <li className='w-full'>
-            <a href='#kontak' className='block'>
+            <Link href='#kontak' className='block' aria-label='link kontak'>
               Kontak
-            </a>
+            </Link>
+          </li>
+          <li className='w-full bg-primary text-white text-center p-2 rounded-xl'>
+            <Link href='/auth/login' className='block' aria-label='masuk'>
+              Masuk
+            </Link>
+          </li>
+          <li className='w-full border p-2 text-center border-primary rounded-xl'>
+            <Link href='/auth/register' className='block' aria-label='daftar'>
+              Daftar
+            </Link>
           </li>
         </ul>
       </nav>
