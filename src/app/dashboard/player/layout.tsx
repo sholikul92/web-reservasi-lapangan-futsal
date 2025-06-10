ec5686da724d30f637e95819e2ddb536af10e0f5
sholikul <sholikulardian@gmail.com>
2025-06-10 19:11:11 +0700

    fix: import module

--- src/app/dashboard/player/layout.tsx
+++ src/app/dashboard/player/layout.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react";
 import { getServerSession } from "next-auth";
-import { authOption } from "@/app/api/auth/[...nextauth]/route";
+import { authOption } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import { SessionContextProvider } from "@/app/context/session-context";
 
