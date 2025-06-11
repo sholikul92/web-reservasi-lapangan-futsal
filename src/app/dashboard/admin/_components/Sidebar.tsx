bac3c7fc56571afa264a18a8bb7331be6d0ef45e
sholikul <sholikulardian@gmail.com>
2025-06-11 12:52:26 +0700

    add logout

--- src/app/dashboard/admin/_components/Sidebar.tsx
+++ src/app/dashboard/admin/_components/Sidebar.tsx
@@ -1,25 +1,35 @@
 "use client";
 import { BiSolidHome } from "react-icons/bi";
-import { FaCalendarAlt, FaHistory, FaSignOutAlt } from "react-icons/fa";
+import { FaCalendarAlt, FaSignOutAlt } from "react-icons/fa";
 import { MdSettings } from "react-icons/md";
 import { TbSoccerField } from "react-icons/tb";
 import { Tooltip } from "@heroui/react";
+import Link from "next/link";
+import { signOut } from "next-auth/react";
 
 export default function Sidebar() {
+  const handleClickLogout = () => {
+    signOut({
+      callbackUrl: "/",
+    });
+  };
   return (
-    <aside className='bg-green-700 text-white md:w-16 min-h-screen p-2 flex fixed flex-col justify-between items-center'>
-      <div className='md:text-3xl space-y-10 flex-1 mt-2 cursor-pointer'>
+    <aside className='bg-green-700 text-white md:w-16 min-h-screen p-2 flex fixed flex-col justify-between items-center z-50'>
+      <div className='md:text-3xl flex flex-col gap-8 flex-1 mt-2 cursor-pointer'>
         <Tooltip content='Dashboard' placement='left'>
-          <BiSolidHome />
+          <Link href='/dashboard/admin'>
+            <BiSolidHome />
+          </Link>
         </Tooltip>
         <Tooltip content='Lapangan' placement='left'>
-          <TbSoccerField />
+          <Link href='/dashboard/admin/lapangan'>
+            <TbSoccerField />
+          </Link>
         </Tooltip>
         <Tooltip content='Jadwal' placement='left'>
-          <FaCalendarAlt />
-        </Tooltip>
-        <Tooltip content='History' placement='left'>
-          <FaHistory />
+          <Link href='/dashboard/admin/jadwal'>
+            <FaCalendarAlt />
+          </Link>
         </Tooltip>
         <Tooltip content='Pengaturan' placement='left'>
           <MdSettings />
@@ -27,7 +37,7 @@ export default function Sidebar() {
       </div>
       <div className='md:text-3xl'>
         <Tooltip content='Keluar' placement='left'>
-          <FaSignOutAlt />
+          <FaSignOutAlt onClick={handleClickLogout} />
         </Tooltip>
       </div>
     </aside>
